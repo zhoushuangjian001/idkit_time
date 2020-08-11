68762ff15036bea3fb563ba6fcbae66a35cc6485
shuangjian.zhou <zhoushuangjian@blhcn.com>
2020-08-11 14:58:32 +0800

    test 的测试

--- test/idkit_time_test.dart
+++ test/idkit_time_test.dart
@@ -4,10 +4,7 @@ import 'package:idkit_time/idkit_time.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    expect(IDKitTime.formatDate(DateTime.parse("2020-08-11 14:41:03")),
+        "2020-08-11 14:41:03");
   });
 }
